a64c45b9fceb35638b0bc2441162c13aa60ba432
kttian <19katherinet@students.harker.org>
2018-09-05 22:39:37 -0700

    single task interpretation

---
+++ results/nandi/bQTL/bQTL_18_08_24/modisco_task2/run_all.sh
@@ -0,0 +1,2 @@
+#$TFNET_ROOT/scripts/run_pipeline.py --tfs JUND,RELA,SPI1,STAT1 --cells GM12878 --end-task 5 --start 10
+$TFNET_ROOT/scripts/run_pipeline.py --tfs JUND,RELA,SPI1,STAT1 --cells GM12878 --start-task 2 --end-task 3 --start 80
---
+++ results/nandi/bQTL/bQTL_18_08_24/modisco_task3/run_all.sh
@@ -0,0 +1,2 @@
+#$TFNET_ROOT/scripts/run_pipeline.py --tfs JUND,RELA,SPI1,STAT1 --cells GM12878 --end-task 5 --start 10
+$TFNET_ROOT/scripts/run_pipeline.py --tfs JUND,RELA,SPI1,STAT1 --cells GM12878 --start-task 3 --end-task 4 --start 80
