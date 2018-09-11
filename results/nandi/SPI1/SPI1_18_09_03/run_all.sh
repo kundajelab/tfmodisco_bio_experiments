8500d4c333165f463c91a206b125e8e981c6c842
kttian <19katherinet@students.harker.org>
2018-09-10 19:40:13 -0700

    SPI1_18_09_03 partial update

--- results/nandi/SPI1/SPI1_18_09_03/run_all.sh
+++ results/nandi/SPI1/SPI1_18_09_03/run_all.sh
@@ -1,2 +1,2 @@
 #$TFNET_ROOT/scripts/run_pipeline.py --tfs SPI1 --start 10 --end 20
-$TFNET_ROOT/scripts/run_pipeline.py --tfs SPI1 --end-task 4 --start 50
+$TFNET_ROOT/scripts/run_pipeline.py --tfs SPI1 --end-task 1 --start 80
