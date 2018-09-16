3f031f066a4a0c180345263a5006056c9548872c
kttian <19katherinet@students.harker.org>
2018-09-15 18:17:02 -0700

    STAT1 udpate run2 --min-seqlets=2800

---
+++ results/nandi/STAT1/STAT1_GM12878_refine_18_09_04/modisco.run2/run_all.sh
@@ -0,0 +1,2 @@
+#$TFNET_ROOT/scripts/run_pipeline.py --tfs STAT1 --cells GM12878 --end-task 1 --start 10
+$TFNET_ROOT/scripts/run_pipeline.py --tfs STAT1 --cells GM12878 --end-task 1 --start 80 --stride 1 --fdr 0.95 --min-seqlets 2800
