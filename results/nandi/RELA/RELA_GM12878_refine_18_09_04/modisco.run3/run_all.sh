a95b4c79845c02d472a695e31c5fae77d397bc99
kttian <19katherinet@students.harker.org>
2018-09-15 17:58:17 -0700

    RELA update run3 --min-seqlets 15000

---
+++ results/nandi/RELA/RELA_GM12878_refine_18_09_04/modisco.run3/run_all.sh
@@ -0,0 +1,2 @@
+#$TFNET_ROOT/scripts/run_pipeline.py --tfs RELA --cells GM12878 --end-task 1 --start 10
+$TFNET_ROOT/scripts/run_pipeline.py --tfs RELA --cells GM12878 --end-task 1 --start 80 --fdr 0.25 --min-seqlets 15000
