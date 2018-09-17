9ce913b9af96a0eafa5e122160a26ecac6a054e7
kttian <19katherinet@students.harker.org>
2018-09-16 18:06:07 -0700

    ZNF143 ENCSR936XTK update single task

---
+++ results/nautilus/ZNF143/ZNF143_GM12878_18_09_14/run_all.sh
@@ -0,0 +1,2 @@
+#$TFNET_ROOT/scripts/run_pipeline.py --tfs ZNF143 --cells GM12878 --end-task 1 --start 10
+$TFNET_ROOT/scripts/run_pipeline.py --tfs ZNF143 --cells GM12878 --expr ENCSR936XTK --end-task 1 --start 50 --stride 20 --pf True
