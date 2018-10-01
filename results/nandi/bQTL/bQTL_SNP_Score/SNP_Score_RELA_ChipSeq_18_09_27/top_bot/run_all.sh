b8b6ef47dba3d267c56ab9e376bf51b2a5b48424
kttian <19katherinet@students.harker.org>
2018-10-01 00:51:50 -0700

    RELA/SPI1 bQTL Chip-seq + ENCODE

---
+++ results/nandi/bQTL/bQTL_SNP_Score/SNP_Score_RELA_ChipSeq_18_09_27/top_bot/run_all.sh
@@ -0,0 +1,18 @@
+cat $TFNET_ROOT/results/nandi/bQTL/analysis/bQTL_all_SNPs/RELA_20k.txt | python $TFNET_ROOT/scripts/snp_to_bed.py 1000 > RELA_20k.tsv
+ln -s RELA_20k.tsv interpret.tsv
+bedtools getfasta -fi $TFNET_ROOT/genome/hg19.fa -bed interpret.tsv -fo interpret.fa
+
+mkdir scores
+python $TFNET_ROOT/scripts/gen_allele.py
+
+mv interpret.fa interpret.fa0
+mv interpret.fa* scores
+ln -s scores/interpret.fa0 interpret.fa
+mkdir logs
+
+ln -s ../../../bQTL_ChipSeq_RELA_18_08_29/model_files .
+#ln -s /home/ktian/kundajelab/tfnet/results/nandi/RELA/RELA_GM12878_refine_18_09_04/model_files .
+#source $TFNET_ROOT/scripts/run_allele_deeplift.sh
+source $TFNET_ROOT/scripts/run_allele_deeplift.sh 0
+
+#python get_scores.py
